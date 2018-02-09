84c4904f0a6cd2acbe11ca9ddd5ebe74876bf84e
nalbam <skfqka@80>
2018-02-09 18:00:02 +0900

    IP Pool

--- src/main/webapp/application/views/include/head.php
+++ src/main/webapp/application/views/include/head.php
@@ -67,7 +67,7 @@
                                 <li><a href="/config"><i class="fa fa-cog"></i> Config</a></li>
                                 <li><a href="/certificate"><i class="fa fa-certificate"></i> Certificate</a></li>
                             <? } ?>
-                            <li><a href="/ip"><i class="fa fa-glass"></i> Ip Pool</a></li>
+                            <li><a href="/ip"><i class="fa fa-glass"></i> IP Pool</a></li>
                             <li><a href="/user"><i class="fa fa-users"></i> User</a></li>
                             <li><a href="/dashboard"><i class="fa fa-dashboard"></i> Dashboard</a></li>
                         </ul>
